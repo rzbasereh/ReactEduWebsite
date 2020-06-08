f848e3d2383a5710fc05e60ae5b888af56c7bc51
sadra467 <m.sad.ra.972@gmail.com>
2020-06-08 00:49:23 -0700

    uguj

--- templates/src/containers/login.js
+++ templates/src/containers/login.js
@@ -8,10 +8,12 @@ import {Card} from 'antd';
 import {Form, Input, Button, Checkbox} from 'antd';
 import {UserOutlined, LockOutlined} from '@ant-design/icons';
 import {Link} from "react-router-dom";
+
 const Login = (props) => {
     const onFinish = values => {
         console.log('Received values of form: ', values);
     };
+
     return (
         <Card className="login-card">
             <Row>
@@ -35,7 +37,10 @@ const Login = (props) => {
                                 <path fillRule="evenodd"
                                       d="M13 14s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002.002zM3.022 13h9.956a.274.274 0 0 0 .014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 0 0 .022.004zm9.974.056v-.002.002zM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                             </svg>
-                            <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="نام کاربری"/>
+                            <Input
+                                prefix={<UserOutlined className="site-form-item-icon"/>}
+                                placeholder="نام کاربری"
+                            />
                         </Form.Item>
                         <Form.Item
                             name="password"
